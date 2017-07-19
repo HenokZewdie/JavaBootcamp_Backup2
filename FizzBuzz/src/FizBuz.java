5b1566f4a2e06783e91473ac0e444a9b121aa05b
HenokZewdie <javabcamp@gmail.com>
2017-07-19 10:48:40 -0400

    Backip from workspace

---
+++ FizzBuzz/src/FizBuz.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FizBuz {
+public static void main(String[] args){
+	
+	
+	int number = 100;
+	for(int i = 1; i<=100; i++){
+	if(i % 3 ==0 && i %5 == 0){
+		
+		System.out.println( "FizzBuzz");}
+	
+		else if (i % 3 ==0){System.out.println( "Fizz");}
+		else if (i %5 == 0){System.out.println( "Buzz");}
+		else{System.out.println(i);}
+	}
+	System.out.println("This is to check github pull on this file");
+	
+	System.out.println("This is for blame the code");
+System.out.println("This is for blame the code");System.out.println("This is for blame the code");
+System.out.println("This is for blame the code");System.out.println("This is for blame the code");
+System.out.println("This is for blame the code");System.out.println("This is for blame the code");
+System.out.println("This is for blame the code");
+System.out.println("This is for blame the code");
+System.out.println("This is for blame the code");
+}
+
+}
